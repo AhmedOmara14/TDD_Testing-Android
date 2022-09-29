57aeb206394444cb8707d0825f7ed6ec69bde187
AhmedOmara14 <ahmedomaea53@gmail.com>
2022-09-29 23:43:41 +0200

    test cases for registeration form

--- app/src/main/java/com/omaradev/unittesting/RegistrationUtil.kt
+++ app/src/main/java/com/omaradev/unittesting/RegistrationUtil.kt
@@ -1,4 +1,20 @@
 package com.omaradev.unittesting
 
-object RegisterationUtil {
+object RegistrationUtil {
+    /**
+     * Test Cases For Register Form
+     * the input is not valid if ...
+     * ... the userName/passWord is empty
+     * ... the user name is already taken
+     * ... password and confirm password is not the the same
+     * ... password contains less than 2 digits
+     **/
+
+    fun validateRegisterInput(
+        userName:String,
+        password:String,
+        confirmedPassword:String
+    ): Boolean {
+        return true
+    }
 }
